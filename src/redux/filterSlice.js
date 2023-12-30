f6a1430aca3bae7d69384842f8076843c1592f78
AnastasiiaZastup <131608774+AnastasiiaZastup@users.noreply.github.com>
2023-12-30 21:47:49 +0200

    commit

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -8,7 +8,7 @@ export const filterSlice = createSlice({
 
   reducers: {
     updateFilter(state, action) {
-      return action.payload;
+      state.filter = action.payload;
     },
   },
 });
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,19 +1,16 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-export const selectContacts = state => state.contacts.items || [];
-export const selectFilter = state => state.filter || '';
-export const selectError = state => state.contacts.error || null;
-export const selectIsLoading = state => state.contacts.isLoading || false;
+export const selectContacts = state => state.contacts.contacts.items;
+export const selectFilter = state => state.filter.filter;
+export const selectError = state => state.contacts.contacts.error;
+export const selectIsLoading = state => state.contacts.contacts.isLoading;
 
 export const selectVisible = createSelector(
   [selectContacts, selectFilter],
   (contacts, filter) => {
-    // Ensure contacts and filter are defined
     if (!contacts || !filter) {
       return [];
     }
-
-    // Filter contacts based on the provided filter
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
